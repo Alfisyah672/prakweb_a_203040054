23d31bdf0e7dfeb4b2a7545f2ab3b245d9701766
AlfiS.R <alfisyahrin673@gmail.com>
2022-10-03 21:37:25 +0700

    property dan method

--- produk.php
+++ produk.php
@@ -2,14 +2,9 @@
 
 class Produk
 {
-  public $judul = 'judul',
-    $penulis = 'penulis',
-    $penerbit = 'penerbit',
-    $harga = 'harga';
 }
 
 
 $produk1 = new Produk();
-$produk1->judul = "Boruto";
-var_dump($produk1);
+
 $produk2 = new Produk();
