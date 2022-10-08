30257416241edc0ac578732e07e01bf371617bc5
AlfiS.R <alfisyahrin673@gmail.com>
2022-10-08 23:52:33 +0700

    Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About
+{
+  public function index($nama = 'Alfi ', $pekerjaan = 'Gamer')
+  {
+    echo "Hallo $nama, saya $pekerjaan";
+  }
+  public function page()
+  {
+    echo 'About/page';
+  }
+}
