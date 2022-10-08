1a529f4176d2cf3ec2623a4ad2b0c6152846fd29
AlfiS.R <alfisyahrin673@gmail.com>
2022-10-09 00:39:17 +0700

    View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php
 
-class About
+class About extends Controller
 {
-  public function index($nama = 'Alfi ', $pekerjaan = 'Gamer')
+  public function index($nama = 'Alfi ', $pekerjaan = 'Gamer', $umur = 20)
   {
-    echo "Hallo $nama, saya $pekerjaan";
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $data['judul'] = 'About me';
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/');
   }
   public function page()
   {
-    echo 'About/page';
+    $data['judul'] = 'pages';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   }
 }
